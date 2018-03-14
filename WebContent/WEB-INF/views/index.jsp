38f9ca5cc613b61186069fe68a6878d437684f0a
bit <bit@DESKTOP-518UE4O>
2018-03-14 13:13:36 +0900

    guestbook - servlet version

---
+++ WebContent/WEB-INF/views/index.jsp
@@ -0,0 +1,54 @@
+<%@page import="com.cafe24.guestbook.vo.GuestbookVO"%>
+<%@page import="java.util.List"%>
+<%@page import="com.cafe24.guestbook.dao.GuestbookDAO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	List<GuestbookVO> list = (List<GuestbookVO>)request.getAttribute( "list" );
+%>
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>방명록</title>
+</head>
+<body>
+	<form action="/guestbook2/board" method="post">
+		<input type="hidden" name="a" value="add" />
+		<table border="1" width="510">
+			<tr>
+				<td>이름</td><td><input type="text" name="name"></td>
+				<td>비밀번호</td><td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td colspan=4><textarea name="content" cols=66 rows=5></textarea></td>
+			</tr>
+			<tr>
+				<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
+			</tr>
+		</table>
+	</form>
+	<br>
+<%
+	if (list != null ) {
+    	for ( GuestbookVO vo : list ) { 
+%>
+	<table width="510" border="1">
+		<tr>
+			<td>[<%= vo.getNo() %>]</td>
+			<td><%= vo.getName() %></td>
+			<td><%= vo.getRegDate() %></td>
+			<td><a href="/guestbook2/board?a=deleteform&no=<%= vo.getNo() %>">삭제</a></td>
+		</tr>
+		<tr>
+			<td colspan="4">
+			<%= vo.getContent().replace("\n", "<br />") %>
+			</td>
+		</tr>
+	</table>
+	<br />
+<%
+		}
+	}
+%>
+</body>
+</html>
---
+++ src/com/cafe24/guestbook/controller/GuestbookServlet.java
@@ -0,0 +1,71 @@
+package com.cafe24.guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cafe24.guestbook.dao.GuestbookDAO;
+import com.cafe24.guestbook.vo.GuestbookVO;
+import com.cafe24.mvc.util.WebUtil;
+
+@WebServlet( "/board" )
+public class GuestbookServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet( HttpServletRequest request, HttpServletResponse response )
+	    throws ServletException, IOException {
+	request.setCharacterEncoding( "UTF-8" );
+
+	String actionName = request.getParameter( "a" );
+	if ( "deleteform".equals( actionName ) ) {
+	    String no = request.getParameter( "no" );
+	    request.setAttribute( "no", no );
+	    WebUtil.forward( request, response, "/WEB-INF/views/deleteform.jsp" );
+	} else if ( "add".equals( actionName ) ) {
+	    String name = request.getParameter( "name" );
+	    String password = request.getParameter( "password" );
+	    String content = request.getParameter( "content" );
+
+	    GuestbookVO vo = new GuestbookVO();
+	    vo.setName( name );
+	    vo.setPassword( password );
+	    vo.setContent( content );
+
+	    GuestbookDAO dao = new GuestbookDAO();
+	    dao.create( vo );
+
+	    WebUtil.redirect( request, response, "/guestbook2/board" );
+	} else if ( "delete".equals( actionName ) ) {
+	    Long no = Long.parseLong(request.getParameter( "no" ));
+		String password = request.getParameter( "password" );
+		String content = request.getParameter( "content" );
+		
+		GuestbookVO vo = new GuestbookVO();
+		vo.setNo( no );
+		vo.setPassword( password );
+		
+		GuestbookDAO dao = new GuestbookDAO();
+		dao.delete( vo );
+		
+		WebUtil.redirect( request, response, "/guestbook2/board" );
+	} else {
+	    /* default action 처리 */
+	    GuestbookDAO dao = new GuestbookDAO();
+	    List<GuestbookVO> list = dao.readAll();
+
+	    request.setAttribute( "list", list );
+	    WebUtil.forward( request, response, "/WEB-INF/views/index.jsp" );
+	}
+    }
+
+    protected void doPost( HttpServletRequest request, HttpServletResponse response )
+	    throws ServletException, IOException {
+	doGet( request, response );
+    }
+
+}
